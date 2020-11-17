2083d7a06f621ee3bf7c3165fd884041ed2a105d
Denis Navarro <denis.navarro@invoicesharing.com>
2020-11-17 15:39:34 +0100

    release: v1.2.8

--- petifun_container/libs/owner/application/services.py
+++
@@ -1,8 +0,0 @@
-from petifun_container.libs.owner.domain.repositories import OwnerRepository
-from petifun_container.libs.shared.application.services import Service
-
-
-class OwnerCreatorService(Service):
-    def __init__(self, repository: OwnerRepository, count: int):
-        self.repository = repository
-        self.count = count
