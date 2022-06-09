596e84edfc51bf8de203ca08f8c95044b36c1c82
ticdenis <runner@fv-az462-581.0zuljvm5d0yedf4jva4xayy4gc.gx.internal.cloudapp.net>
2022-06-09 21:47:32 +0000

    release: v1.3.6

--- aioddd/__init__.py
+++ aioddd/__init__.py
@@ -46,7 +46,7 @@ from .events import (
 from .utils import env, get_env, get_simple_logger
 from .value_objects import Id, StrDateTime, Timestamp
 
-__version__ = '1.3.5'
+__version__ = '1.3.6'
 
 __all__ = (
     # aggregates
